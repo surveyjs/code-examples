3ba3383771a2a01272c491d5c03f311f6b61e371
tsukanov_roman <roman.tsukanov@devexpress.com>
2022-02-21 18:44:49 +0300

    init

---
+++ quiz/jquery/index.js
@@ -0,0 +1,71 @@
+Survey.StylesManager.applyTheme("modern");
+
+const surveyJson = {
+    title: "American History",
+    showProgressBar: "bottom",
+    showTimerPanel: "top",
+    maxTimeToFinishPage: 10,
+    maxTimeToFinish: 25,
+    firstPageIsStarted: true,
+    startSurveyText: "Start Quiz",
+    pages: [{
+        elements: [{
+            type: "html",
+            html: "You are about to start a quiz on American history. <br/>You will have 10 seconds for every question and 25 seconds to end the quiz.<br/>Enter your name below and click <b>Start Quiz</b> to begin."
+        }, {
+            type: "text",
+            name: "name",
+            titleLocation: "hidden",
+            isRequired: true
+        }]
+    }, {
+        elements: [{
+            type: "radiogroup",
+            name: "civilwar",
+            title: "When was the American Civil War?",
+            choices: [
+                "1796-1803", "1810-1814", "1861-1865", "1914-1918", "1939-1945"
+            ],
+            correctAnswer: "1861-1865"
+        }]
+    }, {
+        elements: [{
+            type: "radiogroup",
+            name: "libertyordeath",
+            title: "Whose quote is this: \"Give me liberty, or give me death\"?",
+            choicesOrder: "random",
+            choices: [
+                "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+            ],
+            correctAnswer: "Patrick Henry"
+        }]
+    }, {
+        elements: [{
+            type: "radiogroup",
+            name: "magnacarta",
+            title: "What is Magna Carta?",
+            choicesOrder: "random",
+            choices: [
+                "The foundation of the British parliamentary system",
+                "The Great Seal of the monarchs of England",
+                "The French Declaration of the Rights of Man",
+                "The charter signed by the Pilgrims on the Mayflower"
+            ],
+            correctAnswer: "The foundation of the British parliamentary system"
+        }]
+    }],
+    completedHtml: "<h4>You got <b>{correctedAnswers}</b> out of <b>{questionCount}</b> correct answers.</h4>",
+    completedHtmlOnCondition: [{
+        expression: "{correctedAnswers} == 0",
+        html: "<h4>Unfortunately, none of your answers are correct. Please try again. <br> correctedAnswers: <b>{correctedAnswers}</b>; questionCount: <b>{questionCount}</b></h4>"
+    }, {
+        expression: "{correctedAnswers} == {questionCount}",
+        html: "<h4>Congratulations! You answered all the questions correctly! <br> correctedAnswers: <b>{correctedAnswers}</b>; questionCount: <b>{questionCount}</b></h4>"
+    }] 
+};
+
+const survey = new Survey.Model(surveyJson);
+
+$(function() {
+    $("#surveyContainer").Survey({ model: survey });
+});
---
+++ quiz/knockout/README.md
@@ -0,0 +1,3 @@
+# Create a Quiz (SurveyJS for Knockout)
+
+This example is used in the following help topic: [Create a Quiz](https://surveyjs.io/Documentation/Library?id=design-survey-create-a-quiz). To run the example, open `index.html` in your browser.
