6cfbf2855250d4e727fcaaa1566075271755a4a5
RomanTsukanov <roman.tsukanov@devexpress.com>
2022-03-18 18:21:10 +0300

    Add Angular code example

---
+++ get-started-creator-angular/src/app/survey-creator/survey-creator.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SurveyCreatorComponent } from './survey-creator.component';
+
+describe('SurveyCreatorComponent', () => {
+  let component: SurveyCreatorComponent;
+  let fixture: ComponentFixture<SurveyCreatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SurveyCreatorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SurveyCreatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
