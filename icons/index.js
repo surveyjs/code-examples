04b45a167a47c3261f659d78af175a37f8b755d6
Roman Tsukanov <roman.tsukanov@devexpress.com>
2022-08-04 13:46:24 +0400

    Update Icons example

--- icons/index.js
+++ icons/index.js
@@ -2,10 +2,20 @@ Survey.SvgRegistry.renderIcons();
 
 ReactDOM.render(
   <div>
-    {Object.keys(Survey.SvgRegistry.icons).map(function(name) {
-        return <div><SurveyReact.SvgIcon iconName={"icon-" + name}
-    size={"auto"} className={"sd-detail-panel__icon"}
-    />{"icon-" + name}</div>;
-    })}
-  </div>
-, document.getElementById("creatorElement"));
+    {
+      Object.keys(Survey.SvgRegistry.icons).map(name => {
+        return (
+          <div class="container">
+            <SurveyReact.SvgIcon
+              iconName={"icon-" + name}
+              size={"auto"}
+              className={"sd-detail-panel__icon"}
+            />
+            <span className={"icon-name"}>{"icon-" + name}</span>
+          </div>
+        )
+      })
+    }
+  </div>,
+  document.getElementById("icons")
+);
