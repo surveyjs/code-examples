88523d2f44327a128cb2f12ae7c0eca4b49c5c81
Roman Tsukanov <roman.tsukanov@devexpress.com>
2022-08-05 11:38:56 +0400

    Update the Icons example to use only vanilla js

--- icons/index.js
+++ icons/index.js
@@ -1,21 +1,12 @@
 Survey.SvgRegistry.renderIcons();
 
-ReactDOM.render(
-  <div>
-    {
-      Object.keys(Survey.SvgRegistry.icons).map(name => {
-        return (
-          <div class="container">
-            <SurveyReact.SvgIcon
-              iconName={"icon-" + name}
-              size={"auto"}
-              className={"sd-detail-panel__icon"}
-            />
-            <span className={"icon-name"}>{"icon-" + name}</span>
-          </div>
-        )
-      })
-    }
-  </div>,
-  document.getElementById("icons")
-);
+Object.keys(Survey.SvgRegistry.icons).map(name => {
+  const element = document.createElement("div");
+  element.classList.add("container")
+  element.innerHTML = `
+    <svg class="sd-detail-panel__icon">
+      <use xlink:href="#icon-` + name + `\"></use>
+    </svg>
+    <span class="icon-name">` + name + `</span>`;
+  document.getElementById("icons").appendChild(element);
+});
