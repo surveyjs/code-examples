a3cbff50aee339a5f5e1b4d912459f8c9931b933
Dmitry Kuzin <dk981234@gmail.com>
2023-01-11 12:26:08 +0400

    Refactor angular web service project

--- survey-creator-web-service/angular/src/app/app.component.ts
+++ survey-creator-web-service/angular/src/app/app.component.ts
@@ -7,7 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
-  onClick(event: any) {
-    alert("!");
-  }
 }
--- survey-creator-web-service/angular/src/app/components/creator-widget.component.ts
+++ survey-creator-web-service/angular/src/app/components/creator-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Serializer } from "survey-core";
 import { SurveyCreatorModel } from "survey-creator-core";
@@ -7,49 +7,49 @@ import { getSurveyJSON, getSurveyName, saveSurveyJSON, saveSurveyName } from "..
 Serializer.findProperty("survey", "title").isRequired = true;
 
 @Component({
-    templateUrl: "./creator-widget.component.html",
-    selector: "creator-widget"
+  templateUrl: "./creator-widget.component.html",
+  selector: "creator-widget"
 })
 export class CreatorWidgetComponent implements OnInit {
-    creator!: SurveyCreatorModel;
-    constructor(private route: ActivatedRoute) {
-        
-    }
-    ngOnInit() {
-        const id: number = Number.parseInt(this.route.snapshot.queryParams["id"]);
-        this.creator = new SurveyCreatorModel({ showLogicTab: false })
-        this.creator.isAutoSave = true;
-        //todo search second arg name for callback function
-        this.creator.saveSurveyFunc = (saveNo: number, callback: (saveNo: number, arg: boolean) => void) => {
-            // You can use `this.creator.text` as an alternative to `this.creator.JSON`
-            saveSurveyJSON(id, this.creator.JSON, () => {
-              callback(saveNo, true);
-            });
-          };
-        this.creator.onModified.add((_, options) => {
-            // We are interested in property changes only
-            if (options.type === "PROPERTY_CHANGED") {
-              // Update the survey name in the database when the survey title is changed
-              if (
-                options.name === "title" &&
-                !!options.target &&
-                options.target.getType() === "survey"
-              ) {
-                saveSurveyName(id, options.newValue);
-              }
-            }
-        });
-        getSurveyJSON(id, (json: any) => {
-            // Save the survey title to prevent it from being overwritten
-            const prevTitle = this.creator.survey.title;
-            // You can use `this.creator.text` as an alternative to `this.creator.JSON`
-            this.creator.JSON = json;
-            if (!!prevTitle) {
-              this.creator.survey.title = prevTitle;
-            }
-        });
-        getSurveyName(id, (name: string) => {
-            this.creator.survey.title = name;
-        });
-    }
+  creator!: SurveyCreatorModel;
+  constructor(private route: ActivatedRoute) {
+
+  }
+  ngOnInit() {
+    const id: number = Number.parseInt(this.route.snapshot.queryParams["id"]);
+    this.creator = new SurveyCreatorModel({ showLogicTab: false })
+    this.creator.isAutoSave = true;
+    //todo search second arg name for callback function
+    this.creator.saveSurveyFunc = (saveNo: number, callback: (saveNo: number, arg: boolean) => void) => {
+      // You can use `this.creator.text` as an alternative to `this.creator.JSON`
+      saveSurveyJSON(id, this.creator.JSON, () => {
+        callback(saveNo, true);
+      });
+    };
+    this.creator.onModified.add((_, options) => {
+      // We are interested in property changes only
+      if (options.type === "PROPERTY_CHANGED") {
+        // Update the survey name in the database when the survey title is changed
+        if (
+          options.name === "title" &&
+          !!options.target &&
+          options.target.getType() === "survey"
+        ) {
+          saveSurveyName(id, options.newValue);
+        }
+      }
+    });
+    getSurveyJSON(id, (json: any) => {
+      // Save the survey title to prevent it from being overwritten
+      const prevTitle = this.creator.survey.title;
+      // You can use `this.creator.text` as an alternative to `this.creator.JSON`
+      this.creator.JSON = json;
+      if (!!prevTitle) {
+        this.creator.survey.title = prevTitle;
+      }
+    });
+    getSurveyName(id, (name: string) => {
+      this.creator.survey.title = name;
+    });
+  }
 } 
