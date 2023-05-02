c1c677afa61658f7835ee73a3cb67267fe5c352d
Roman Tsukanov <roman.tsukanov@devexpress.com>
2023-05-02 13:16:22 +0400

    Update Third-Party Angular Component Integration code

--- integrate-third-party-angular-components/src/app/app.component.ts
+++ integrate-third-party-angular-components/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  static declaration = [ColorPickerComponent];
 }
