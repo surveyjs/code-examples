ccab2365a172f8fd24e84e4bc627d32666bee72a
Roman Tsukanov <roman.tsukanov@devexpress.com>
2023-08-14 11:02:46 +0400

    Update data requests to Fetch API

--- get-started-creator/angular/src/app/survey-creator/survey-creator.component.ts
+++ get-started-creator/angular/src/app/survey-creator/survey-creator.component.ts
@@ -47,14 +47,21 @@ export class SurveyCreatorComponent implements OnInit {
 }
 
 // function saveSurveyJson(url: string | URL, json: object, saveNo: number, callback: Function) {
-//   const request = new XMLHttpRequest();
-//   request.open('POST', url);
-//   request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-//   request.addEventListener('load', () => {
+//   fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json;charset=UTF-8'
+//     },
+//     body: JSON.stringify(json)
+//   })
+//   .then(response => {
+//     if (response.ok) {
 //       callback(saveNo, true);
-//   });
-//   request.addEventListener('error', () => {
+//     } else {
 //       callback(saveNo, false);
+//     }
+//   })
+//   .catch(error => {
+//     callback(saveNo, false);
 //   });
-//   request.send(JSON.stringify(json));
 // }
