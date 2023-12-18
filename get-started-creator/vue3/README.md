6bea84abbcf87de1241859e5d43daf97e68c9351
RomanTsukanov <sergeich16@gmail.com>
2023-12-18 14:42:41 +0400

    Add code example for "Get Started with Survey Creator for Vue 3"

---
+++ get-started-creator/vue3/README.md
@@ -0,0 +1,15 @@
+# Get Started with Survey Creator / Form Builder - Vue 3
+
+This example is used in the following help topic: [Add a Survey to a Vue 3 Application](https://surveyjs.io/survey-creator/documentation/get-started-vue#add-survey-creator-to-a-vue-3-application).
+
+## Project setup
+
+```
+npm install
+```
+
+## Compile and hot-reload for development
+
+```
+npm run dev
+```
---
+++ get-started-creator/vue3/src/components/SurveyCreator.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import 'survey-core/defaultV2.min.css';
+import "survey-creator-core/survey-creator-core.min.css";
+
+import type { ICreatorOptions } from "survey-creator-core";
+import { SurveyCreatorModel } from "survey-creator-core";
+
+const creatorOptions: ICreatorOptions = {
+  showLogicTab: true,
+  isAutoSave: true
+};
+
+const defaultJson = {
+  pages: [{
+    name: "Name",
+    elements: [{
+      name: "FirstName",
+      title: "Enter your first name:",
+      type: "text"
+    }, {
+      name: "LastName",
+      title: "Enter your last name:",
+      type: "text"
+    }]
+  }]
+};
+
+const creator = new SurveyCreatorModel(creatorOptions);
+creator.text = window.localStorage.getItem("survey-json") || JSON.stringify(defaultJson);
+creator.saveSurveyFunc = (saveNo: number, callback: Function) => { 
+  window.localStorage.setItem("survey-json", creator.text);
+  callback(saveNo, true);
+  // saveSurveyJson(
+  //     "https://your-web-service.com/",
+  //     creator.JSON,
+  //     saveNo,
+  //     callback
+  // );
+};
+creator.onUploadFile.add((_, options) => {
+  const formData = new FormData();
+  options.files.forEach((file: File) => {
+    formData.append(file.name, file);
+  });
+  fetch("https://example.com/uploadFiles", {
+    method: "post",
+    body: formData
+  }).then(response => response.json())
+    .then(result => {
+      options.callback(
+        "success",
+        // A link to the uploaded file
+        "https://example.com/files?name=" + result[options.files[0].name]
+      );
+    })
+    .catch(error => {
+      options.callback('error');
+    });
+});
+
+// function saveSurveyJson(url: string, json: object, saveNo: number, callback: Function) {
+//   fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json;charset=UTF-8'
+//     },
+//     body: JSON.stringify(json)
+//   })
+//   .then(response => {
+//     if (response.ok) {
+//       callback(saveNo, true);
+//     } else {
+//       callback(saveNo, false);
+//     }
+//   })
+//   .catch(error => {
+//     callback(saveNo, false);
+//   });
+// }
+</script>
+
+<template>
+  <SurveyCreatorComponent :model="creator" />
+</template>
