0dd53e969248bb6a7c4032f365e1099b649b87c0
RomanTsukanov <sergeich16@gmail.com>
2024-01-11 16:35:37 +0400

    Add code for the Third-Party Vue 3 Component Integration tutorial

---
+++ integrate-third-party-vue-components/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import SurveyCreator from './components/SurveyCreator.vue';
+</script>
+
+<template>
+  <div id="surveyCreator">
+    <SurveyCreator />
+  </div>
+</template>
+
+<style scoped>
+#surveyCreator {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ integrate-third-party-vue-components/src/components/SurveyCreator.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import 'survey-core/defaultV2.min.css';
+import "survey-creator-core/survey-creator-core.min.css";
+
+import { Serializer } from "survey-core";
+import { SurveyCreatorModel } from "survey-creator-core";
+import type { SurveyModel } from "survey-core";
+import type { CreatorBase } from "survey-creator-core";
+
+Serializer.addProperty("survey", {
+  name: "backgroundColor",
+  displayName: "Background color",
+  type: "color",
+  category: "general",
+  visibleIndex: 3,
+  onSetValue: (survey: SurveyModel, value: string) => {
+    survey.setPropertyValue("backgroundColor", value);
+    applyBackground(value);
+  }
+});
+
+const surveyJson = {
+  elements: [{
+    type: "color-picker",
+    name: "question1",
+    title: "Pick a color",
+    colorPickerType: "Sketch"
+  }]
+};
+
+function applyBackground(color: string) {
+  setTimeout(() => {
+    const surveyEl = document.getElementsByClassName("sd-root-modern")[0] as HTMLElement;
+    if (surveyEl) {
+      surveyEl.style.setProperty("--background", color);
+    }
+  }, 50);
+}
+
+function handleActiveTabChange(sender: CreatorBase, { tabName }: { tabName: string }) {
+  if (tabName === "test" || tabName === "designer") {
+    applyBackground(sender.survey.backgroundColor);
+  }
+}
+
+const creator = new SurveyCreatorModel({});
+creator.onActiveTabChanged.add(handleActiveTabChange);
+creator.JSON = surveyJson;
+</script>
+
+<template>
+  <SurveyCreatorComponent :model="creator" />
+</template>
