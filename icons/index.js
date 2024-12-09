4bcb8b5bf60af2c2910fe9a0b7f65e55453fd972
Dmitry Kurmanov <kurmanov.work@gmail.com>
2024-12-09 15:32:46 +0400

    Update index.js

--- icons/index.js
+++ icons/index.js
@@ -1,24 +1,26 @@
-if (typeof SurveyCreatorCore !== "undefined") {
-  new SurveyCreatorCore.SurveyCreatorModel({}); // Register Survey Creator icons
-}
-
-function renderIcons() {
-  let iconsDiv = document.createElement("div");
-  iconsDiv.id = "sv-icon-holder-global-container";
-  iconsDiv.innerHTML = "<svg>" + Survey.SvgRegistry.iconsRenderedHtml() + "</svg>";
-  iconsDiv.style.display = "none";
-  document.head.appendChild(iconsDiv);
-}
-renderIcons();
-
-Object.keys(Survey.SvgRegistry.icons).map(name => {
-  if (name.indexOf("pg-v1") > -1) return;
-  const element = document.createElement("div");
-  element.classList.add("container")
-  element.innerHTML = `
-    <svg>
-      <use xlink:href="#icon-` + name + `\"></use>
-    </svg>
-    <span class="icon-name">icon-` + name + `</span>`;
-  document.getElementById("icons").appendChild(element);
+document.addEventListener("DOMContentLoaded", (event) => {
+    if (typeof SurveyCreatorCore !== "undefined") {
+    new SurveyCreatorCore.SurveyCreatorModel({}); // Register Survey Creator icons
+  }
+  
+  function renderIcons() {
+    let iconsDiv = document.createElement("div");
+    iconsDiv.id = "sv-icon-holder-global-container";
+    iconsDiv.innerHTML = "<svg>" + Survey.SvgRegistry.iconsRenderedHtml() + "</svg>";
+    iconsDiv.style.display = "none";
+    document.head.appendChild(iconsDiv);
+  }
+  renderIcons();
+  
+  Object.keys(Survey.SvgRegistry.icons).map(name => {
+    if (name.indexOf("pg-v1") > -1) return;
+    const element = document.createElement("div");
+    element.classList.add("container")
+    element.innerHTML = `
+      <svg>
+        <use xlink:href="#icon-` + name + `\"></use>
+      </svg>
+      <span class="icon-name">icon-` + name + `</span>`;
+    document.getElementById("icons").appendChild(element);
+  });
 });
